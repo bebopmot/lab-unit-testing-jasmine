2d2006e17bb7f78b7c34636dd555ffa9aa7366f0
bebopmot <bebopmotinc@gmail.com>
2025-04-06 18:53:29 +0200

    lab solved

--- src/divide.js
+++ src/divide.js
@@ -1,3 +1,8 @@
-function divide () {
-       
+function divide (num1,num2) {
+   
+    if (typeof a !== "number" || typeof b !== "number") {
+    return undefined
+   }  const result = num1 / num2
+   return result
 }
+divide(20,4)
